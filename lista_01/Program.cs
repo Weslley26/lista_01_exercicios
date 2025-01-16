0be043afbe78cef792dc41f806b109b6d4f6aed2
WESLEY DA SILVA DE JESUS <wesley.sjesus7@senacsp.edu.br>
2025-01-16 14:52:13 -0300

    primeiro commit

---
+++ lista_01/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+o Descrição: Crie um programa que peça ao usuário para digitar um número 
+inteiro. O programa deve verificar se o número é par ou ímpar. Utilize a 
+condição if para determinar o resultado e exiba uma mensagem apropriada.
+o Exemplo de Saída: "O nú 1. Verificação de Paridade
+o Objetivo: Praticar o uso do operador % (módulo) e estruturas condicionais 
+simples.
+mero é par." ou "O número é ímpar
+*/
